285671b13a4f664fbee6067bf85d7fddaceb2224
AndreySunstrider <plaksinandrey94@gmail.com>
2021-04-12 20:20:34 +0300

    feature: created convert fragment and rates dialog layouts,added theming
    * created fragment_converter.xml, fragment_rates_dialog.xml
    * created ConverterFragment.kt and chosen as start destination
    * added necessary values(colors, dimens, strings, etc.), styles and drawables
    
    [CTP-003]

---
+++ app/src/main/java/com/andrewsunstrider/convertertest/features/converter/ConverterFragment.kt
@@ -0,0 +1,6 @@
+package com.andrewsunstrider.convertertest.features.converter
+
+import androidx.fragment.app.Fragment
+import com.andrewsunstrider.convertertest.R
+
+class ConverterFragment : Fragment(R.layout.fragment_converter)
