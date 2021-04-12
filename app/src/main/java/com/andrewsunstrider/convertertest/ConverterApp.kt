32a4c04ce2633f970eaf041eee44dad56cf9fb09
AndreySunstrider <plaksinandrey94@gmail.com>
2021-04-12 21:38:59 +0300

    feature: added DI
    * added DI scopes, modules and components
    
    [CTP-004]

--- app/src/main/java/com/andrewsunstrider/convertertest/ConverterApp.kt
+++ app/src/main/java/com/andrewsunstrider/convertertest/ConverterApp.kt
@@ -1,6 +1,10 @@
 package com.andrewsunstrider.convertertest
 
 import android.content.Context
+import com.andrewsunstrider.convertertest.di.components.CoreComponent
+import com.andrewsunstrider.convertertest.di.components.DaggerAppComponent
+import com.andrewsunstrider.convertertest.di.components.DaggerCoreComponent
+import com.andrewsunstrider.convertertest.di.modules.ContextModule
 import com.google.android.play.core.splitcompat.SplitCompatApplication
 
 /**
@@ -10,6 +14,19 @@ import com.google.android.play.core.splitcompat.SplitCompatApplication
  */
 class ConverterApp : SplitCompatApplication() {
 
+    lateinit var coreComponent: CoreComponent
+
+    companion object {
+
+        /**
+         * Obtain core dagger component.
+         *
+         * @param context The application context
+         */
+        @JvmStatic
+        fun coreComponent(context: Context) =
+            (context.applicationContext as? ConverterApp)?.coreComponent
+    }
 
     /**
      * Called when the application is starting, before any activity, service, or receiver objects
@@ -19,5 +36,32 @@ class ConverterApp : SplitCompatApplication() {
      */
     override fun onCreate() {
         super.onCreate()
+        initCoreDependencyInjection()
+        initAppDependencyInjection()
+    }
+
+    // ============================================================================================
+    //  Private init methods
+    // ============================================================================================
+
+    /**
+     * Initialize app dependency injection component.
+     */
+    private fun initAppDependencyInjection() {
+        DaggerAppComponent
+            .builder()
+            .coreComponent(coreComponent)
+            .build()
+            .inject(this)
+    }
+
+    /**
+     * Initialize core dependency injection component.
+     */
+    private fun initCoreDependencyInjection() {
+        coreComponent = DaggerCoreComponent
+            .builder()
+            .contextModule(ContextModule(this))
+            .build()
     }
 }
---
+++ app/src/main/java/com/andrewsunstrider/convertertest/di/scopes/FeatureScope.kt
@@ -0,0 +1,10 @@
+package com.andrewsunstrider.convertertest.di.scopes
+
+import javax.inject.Scope
+
+/**
+ * Scope for a feature module.
+ */
+@Scope
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class FeatureScope
